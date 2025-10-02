698fd5dd3db00d444c5c94a0c8572ca1996d56eb
Yann Vallotton <vallottonyann@gmail.com>
2025-10-02 12:10:22 +0200

    CLeaning express

--- tmp_policy.sql
+++
@@ -1,8 +0,0 @@
-drop policy if exists "temp_client_conversation_whitelist" on public.chat_messages;
-create policy "temp_client_conversation_whitelist"
-  on public.chat_messages
-  for select
-  using (
-    auth.uid() = '697fdd96-c25d-4462-8939-bde6a98b42cb'
-    and conversation_id = '600a0388-f6a0-4e51-a8c1-9f5e5b3e6629'
-  );
--- tmp_realtime_check.sql
+++
@@ -1,5 +0,0 @@
-set local role authenticated;
-select set_config('request.jwt.claims', '{"sub":"697fdd96-c25d-4462-8939-bde6a98b42cb","email":"vallottonyann@gmail.com","role":"authenticated"}', true);
-select auth.uid();
-select get_current_user_email();
-select id, conversation_id, sender, content from public.chat_messages where conversation_id = '600a0388-f6a0-4e51-a8c1-9f5e5b3e6629' limit 3;
